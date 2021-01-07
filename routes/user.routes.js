40629806052424f78c840b143fb31538162a0037
elvisGomes <55617418+elvisGomes@users.noreply.github.com>
2021-01-07 15:15:27 +0100

    Ressource et commentaire éffectuer

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const authController = require('../controllers/auth.controller');
 const userController = require('../controllers/user.controller');
 
+
 //auth
 router.post("/register", authController.signUp);
 router.post("/login", authController.signIn);
